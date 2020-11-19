383865ab4887ad9ca928075de7ae53bff68c3f49
kbh1860 <60511718+kbh1860@users.noreply.github.com>
2020-11-20 01:55:01 +0900

    Add files via upload

---
+++ AutoClick.py
@@ -0,0 +1,5 @@
+import pyautogui
+
+pyautogui.moveTo(4294, 668, 2) # setting postion where you want on click (x, y, moving time)
+
+pyautogui.click(clicks=3574) # setting number that how much you wants click
